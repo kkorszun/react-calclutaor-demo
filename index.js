0f8bf6130219c0a988f34b99c3da1109d624ae1e
Kacper Korszuń <kkorszun@outlook.com>
2020-03-06 16:20:34 +0100

    add components structure

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
+function OperationButton(props){
+  return(
+    <button>{props.value}</button>
+  );
+}
+
+function NumberButton(props){
+  return(
+  <button>{props.value}</button>
+  );
+}
+
+function PointButton(props){
+  return(
+    <button>.</button>
+  );
+}
+
+function EqualButton(props){
+  return(
+    <button>=</button>
+  )
+}
+
+function Display(props) {
+  return(<p>{ props.value }</p>)
+}
+
+function Calculator(props){
+  return(
+    <div className="react-calculator">
+      <table>
+        <tbody>
+        <tr><td colSpan={4}><Display value="0"/></td></tr>
+        <tr>
+          <td><NumberButton value={1}/></td>
+          <td><NumberButton value={2}/></td>
+          <td><NumberButton value={3}/></td>
+          <td><OperationButton value="÷"/></td>
+        </tr>
+        <tr>
+          <td><NumberButton value={4}/></td>
+          <td><NumberButton value={5}/></td>
+          <td><NumberButton value={6}/></td>
+          <td><OperationButton value="×"/></td>
+        </tr>
+        <tr>
+          <td><NumberButton value={7}/></td>
+          <td><NumberButton value={8}/></td>
+          <td><NumberButton value={9}/></td>
+          <td><OperationButton value="-"/></td>
+        </tr>
+        <tr>
+          <td><NumberButton value={0}/></td>
+          <td><PointButton/></td>
+          <td><EqualButton/></td>
+          <td><OperationButton value="+"/></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+
 ReactDOM.render(
-    <p>Hello world2!</p>, 
+    <Calculator/>, 
     document.getElementById('root')
   );
