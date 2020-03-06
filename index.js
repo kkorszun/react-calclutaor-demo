561600fdc556f95ee5935b04a8bf888f55935585
Kacper Korszuń <kkorszun@outlook.com>
2020-03-06 18:50:54 +0100

    add display logic

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function OperationButton(props){
 
 function NumberButton(props){
   return(
-  <button>{props.value}</button>
+  <button value={props.value} onClick={props.onClick}>{props.value}</button>
   );
 }
 
@@ -26,40 +26,58 @@ function Display(props) {
   return(<p>{ props.value }</p>)
 }
 
-function Calculator(props){
-  return(
+class Calculator extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      leftSide: props.value ? props.value : 0,
+      rightSide: 0,
+      isLeftSide: true,
+      displayState: props.value ? props.value.toString() : "0",
+    };
+    this.onNumberClick = this.onNumberClick.bind(this);
+  }
+
+  onNumberClick(e){
+    let newState = this.state.displayState+e.target.value.toString();
+    if(this.state.displayState == "0") {
+      newState =  e.target.value.toString();
+    }
+    this.setState({displayState: newState})
+  }
+
+  render(){return(
     <div className="react-calculator">
       <table>
         <tbody>
-        <tr><td colSpan={4}><Display value="0"/></td></tr>
+        <tr><td colSpan={4}><Display value={this.state.displayState}/></td></tr>
         <tr>
-          <td><NumberButton value={1}/></td>
-          <td><NumberButton value={2}/></td>
-          <td><NumberButton value={3}/></td>
+          <td><NumberButton value={1} onClick={this.onNumberClick}/></td>
+          <td><NumberButton value={2} onClick={this.onNumberClick}/></td>
+          <td><NumberButton value={3} onClick={this.onNumberClick}/></td>
           <td><OperationButton value="÷"/></td>
         </tr>
         <tr>
-          <td><NumberButton value={4}/></td>
-          <td><NumberButton value={5}/></td>
-          <td><NumberButton value={6}/></td>
+          <td><NumberButton value={4} onClick={this.onNumberClick}/></td>
+          <td><NumberButton value={5} onClick={this.onNumberClick}/></td>
+          <td><NumberButton value={6} onClick={this.onNumberClick}/></td>
           <td><OperationButton value="×"/></td>
         </tr>
         <tr>
-          <td><NumberButton value={7}/></td>
-          <td><NumberButton value={8}/></td>
-          <td><NumberButton value={9}/></td>
+          <td><NumberButton value={7} onClick={this.onNumberClick}/></td>
+          <td><NumberButton value={8} onClick={this.onNumberClick}/></td>
+          <td><NumberButton value={9} onClick={this.onNumberClick}/></td>
           <td><OperationButton value="-"/></td>
         </tr>
         <tr>
-          <td><NumberButton value={0}/></td>
+          <td><NumberButton value={0} onClick={this.onNumberClick}/></td>
           <td><PointButton/></td>
           <td><EqualButton/></td>
           <td><OperationButton value="+"/></td>
         </tr>
         </tbody>
       </table>
-    </div>
-  );
+    </div>);}
 }
 
 
