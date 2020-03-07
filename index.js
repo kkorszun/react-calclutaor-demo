0a18ba978659ff9ec67edd128cdbe5384503dde4
Kacper Korszuń <kkorszun@outlook.com>
2020-03-07 18:13:31 +0100

    add simple operation logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function OperationButton(props){
   return(
-    <button>{props.value}</button>
+    <button value={props.value} onClick={(e)=>props.onClick(e,props.operation)}>{props.value}</button>
   );
 }
 
@@ -18,7 +18,7 @@ function PointButton(props){
 
 function EqualButton(props){
   return(
-    <button>=</button>
+    <button onClick={props.onClick}>=</button>
   )
 }
 
@@ -34,8 +34,11 @@ class Calculator extends React.Component {
       rightSide: 0,
       isLeftSide: true,
       displayState: props.value ? props.value.toString() : "0",
+      currentOpeartion: null
     };
     this.onNumberClick = this.onNumberClick.bind(this);
+    this.onOperationClick = this.onOperationClick.bind(this);
+    this.onEqualClick= this.onEqualClick.bind(this);
   }
 
   onNumberClick(e){
@@ -46,6 +49,36 @@ class Calculator extends React.Component {
     this.setState({displayState: newState})
   }
 
+ 
+
+  onOperationClick(e,operation){
+    const parsedDisplay = parseFloat(this.state.displayState);
+    const newLeftSide =  parsedDisplay ? parsedDisplay : 0;
+    this.setState({
+      displayState:"0",
+      rightSide: 0,
+      leftSide: newLeftSide,
+      currentOpeartion: operation,
+      isLeftSide: false,
+    });
+  }
+
+  onEqualClick(e){
+    const parsedDisplay = parseFloat(this.state.displayState);
+    const newRightSide =  parsedDisplay ? parsedDisplay : 0;
+    const currOperation = this.state.currentOpeartion;
+    const operation = currOperation ? currOperation : (x,y) => 0;
+    const result = operation(this.state.leftSide, newRightSide);
+    this.setState({
+      leftSide: result,
+      rightSide: newRightSide,
+      isLeftSide:true,
+      displayState:result.toString()
+    });
+  }
+
+  
+
   render(){return(
     <div className="react-calculator">
       <table>
@@ -55,25 +88,25 @@ class Calculator extends React.Component {
           <td><NumberButton value={1} onClick={this.onNumberClick}/></td>
           <td><NumberButton value={2} onClick={this.onNumberClick}/></td>
           <td><NumberButton value={3} onClick={this.onNumberClick}/></td>
-          <td><OperationButton value="÷"/></td>
+          <td><OperationButton value="÷" onClick={this.onOperationClick} operation={(x,y)=> x/y}/></td>
         </tr>
         <tr>
           <td><NumberButton value={4} onClick={this.onNumberClick}/></td>
           <td><NumberButton value={5} onClick={this.onNumberClick}/></td>
           <td><NumberButton value={6} onClick={this.onNumberClick}/></td>
-          <td><OperationButton value="×"/></td>
+          <td><OperationButton value="×" onClick={this.onOperationClick} operation={(x,y)=> x*y}/></td>
         </tr>
         <tr>
           <td><NumberButton value={7} onClick={this.onNumberClick}/></td>
           <td><NumberButton value={8} onClick={this.onNumberClick}/></td>
           <td><NumberButton value={9} onClick={this.onNumberClick}/></td>
-          <td><OperationButton value="-"/></td>
+          <td><OperationButton onClick={this.onOperationClick} value="-" operation={(x,y)=> x-y}/></td>
         </tr>
         <tr>
           <td><NumberButton value={0} onClick={this.onNumberClick}/></td>
           <td><PointButton/></td>
-          <td><EqualButton/></td>
-          <td><OperationButton value="+"/></td>
+          <td><EqualButton onClick={this.onEqualClick}/></td>
+          <td><OperationButton onClick={this.onOperationClick} value="+" operation={(x,y)=> x+y}/></td>
         </tr>
         </tbody>
       </table>
